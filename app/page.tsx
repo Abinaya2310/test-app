04a08c231469a1e633c9b604cbfbb910c164c8d9
Abinaya S <abi23.subramanian@gmail.com>
2024-05-20 12:11:34 +0530

    new changes1

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 
 
+ function App() {
+   return (
+     <div>
+       <header>
+        
+         <h1>AbinayaS</h1>
+         <p>
+           Fundamental React concepts you will need for almost any app you are
+           going to build!
+         </p>
+       </header>
+       <main>
+         <h2>Time to get started!</h2>
+       </main>
+     </div>
+   );
+ }
+
+ export default App;
 
 
 
 
-
-export default function Home() {
-return (
-   <main><h1>Hello World</h1></main>
-    
-   
-  )
-}
