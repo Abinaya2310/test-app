0a6482503f13038c3d8d5e208c9eea62a6ef0646
Abinaya S <abi23.subramanian@gmail.com>
2024-05-24 17:12:19 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,9 @@
-
-
-
-
-
-
-
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
+function genRandomInt(max: number): number {
   return Math.floor(Math.random() * (max + 1));
 }
+
 
 function Header() {
   return (
@@ -17,7 +11,7 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+       {1+1} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,61 @@
 
 
- function App() {
-   return (
-     <div>
-       <header>
-        
-         <h1>AbinayaS</h1>
-         <p>
-           Fundamental React concepts you will need for almost any app you are
-           going to build!
-         </p>
-       </header>
-       <main>
-         <h2>Time to get started!</h2>
-       </main>
-     </div>
-   );
- }
-
- export default App;
 
 
 
+import componentsImg from '../app/src/assets/components.png';
 
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max: number): number {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+    
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
