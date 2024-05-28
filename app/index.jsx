f523c9a801337b13d9ecde27fa7a653fb9849776
Abinaya S <abi23.subramanian@gmail.com>
2024-05-28 14:35:27 +0530

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+import "page.tsx";
 import "./global.css";
+
+
+
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App/>);
--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,106 @@
 
 
-import componentsImg from './src/assets/components.png';
-import propsImg from './src/assets/config.png';
-import jsxImg from "./src/assets/jsx-ui.png";
-import stateImg from "./src/assets/state-mgmt.png";
-import TabButton from './TabButton';
+
+
+
+"use client";
+
+import { useState } from 'react';
+import { EXAMPLES } from './data';
+import TabButton from "./TabButton";
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-function genRandomInt(max: number): number {
-return Math.floor(Math.random() * (max + 1));
+
+function genRandomInt(max) {
+ return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-const description = reactDescriptions[genRandomInt(2)];
+ const description = reactDescriptions[genRandomInt(2)];
 
-return (
-<header>
-
-<h1>React Essentials</h1>
-<p>
-{description} React concepts you will need for almost any app you are
-going to build!
+ return (
+ <header>
+ <h1>React Essentials</h1>
+ <p>
+ {description} React concepts you will need for almost any app you are going to build!
 </p>
-</header> );
+ </header>
+ );
 }
 
-function CoreConcept(props) {
-return (
-<li>
-<img src={props.image} alt={props.title} />
-<h3>{props.title}</h3>
-<p>{props.description}</p>
-</li>
-);
+function CoreConcept({ title, description }) {
+ return (
+ <li>
+ 
+ <h3>{title}</h3>
+ <p>{description}</p>
+ </li>
+ );
 }
 
 function App() {
-return (
-<div>
-<Header />
-<main>
+ const [selectedTopic, setSelectedTopic] = useState('components');
+
+ function handleSelect(selectedButton) {
+ if (EXAMPLES[selectedButton]) {
+ setSelectedTopic(selectedButton);
+ } else {
+ console.error(`Invalid topic selected: ${selectedButton}`);
+ }
+ }
+
+console.log('APP COMPONENT EXECUTING');
+
+ const selectedExample = EXAMPLES[selectedTopic];
+
+ return (
+ <div>
+ <Header />
+ <main>
 <section id="core-concepts">
 <h2>Core Concepts</h2>
-<ul>
-<CoreConcept
-title="Components"
-description="The core UI building block- compose the user interface by combining multiple components."
-image={componentsImg}
-/>
-<CoreConcept 
-title="Props" 
-description="Make components configurable (and therefore reusable) by passing input data to them."
-image={propsImg}
-/>
+ <ul>
+ <CoreConcept title="Components" description="The core UI building block - compose the user interface by combining multiple components.">
 
-<CoreConcept 
-title="JSX"
-description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered."
-image={jsxImg}/>
+ </CoreConcept>
+ <CoreConcept title="Props" description="Make components configurable (and therefore reusable) by passing input data to them.">
 
-<CoreConcept title="stateImg"
-description=" React-managed data which, when changed, causes the component to re-render & the UI to update."
-image={stateImg}
-/>
+ </CoreConcept>
+<CoreConcept title="JSX" description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.">
 
+ </CoreConcept>
+<CoreConcept title="State" description="React-managed data which, when changed, causes the component to re-render & the UI to update.">
+
+ </CoreConcept>
 </ul>
-</section>
+ </section>
 
-<section id="examples">
+ <section id="examples">
  <h2>Examples</h2>
- <menu>
- <TabButton>Components</TabButton>
- <TabButton>JSX</TabButton>
- <TabButton>Props</TabButton>
- <TabButton>State</TabButton>
-</menu>
+<menu>
+<TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+ <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+ <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+ <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+ </menu>
 
-</section>
+ <div id="tab-content">
+ {selectedExample ? (
+ <>
+<h3>{selectedExample.title}</h3>
+<p>{selectedExample.description}</p>
+ <pre>
+ <code>{selectedExample.code}</code>
+ </pre>
+ </>
+ ) : (
+ <p>Please select a topic.</p>
+ )}
+ </div>
+ </section>
 </main>
-</div>
-);
+ </div>
+ );
 }
 
-
-
-
 export default App;
-
-			
